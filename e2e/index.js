85cce9352d81a7dd2f13f455a65b9cd353ed991e
Utsunomiya Yoichi (宇都宮 洋一) <youichi.utsunomiya@jp.ricoh.com>
2021-11-15 13:30:13 +0900

    ver

--- e2e/index.js
+++ e2e/index.js
@@ -6,7 +6,7 @@ const opts = {
   port: 4723,
   capabilities: {
     platformName: "Android",
-    platformVersion: "11",
+    platformVersion: "9",
     deviceName: "emulator-5554",
     browserName: "Chrome",
     automationName: "UiAutomator2"
